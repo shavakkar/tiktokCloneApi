e20a2f15fae1178278b666e24aaa2bd4fe03ead0
shavakkar <shavakkar@gmail.com>
2024-02-11 15:46:09 +0530

    added many

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AllPostsCollection;
+use App\Http\Resources\UsersCollection;
+
+class ProfileController extends Controller
+{
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try {
+            $posts = Post::where('user_id', $id)->latest()->get();
+            $user = User::where('id', $id)->get();
+
+            return response()->json([
+                'posts' => new AllPostsCollection($posts),
+                'user' => new UsersCollection($user),
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+}
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Services\FileService;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UsersCollection;
 
@@ -33,17 +34,40 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function updateUserImage(Request $request)
     {
-        //
+        $request->validate([
+            'image' => 'required|mimes:png.jpg,pjeg'
+        ]);
+        if ($request->height === '' || $request->width === '' || $request->top === '' || $request->left === '') {
+            return response()->json(['error' => 'The Dimensions are incomplete'], 400);
+        }
+
+        try {
+            $user = (new FileService)->updateImage(auth()->user(), $request);
+            $user->save();
+
+            return response()->json(['success' => 'OK'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function getUser($id)
     {
-        //
+        try {
+            $user = User::findOrFail($id);
+
+            return response()->json([
+                'succes' => 'OK',
+                'user' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
     }
 
     /**
@@ -57,9 +81,23 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function updateUser(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        try {
+            $user = User::findOrFail(auth()->user()->id);
+
+            $user->name = $request->input('name');
+            $user->bio = $request->input('bio');
+            $user->save();
+
+            return response()->json(['success' => 'OK'], 200);
+        } catch (\Exception $e) {
+            response()->json(['error' => $e->getMessage()], 400);
+        }
     }
 
     /**
